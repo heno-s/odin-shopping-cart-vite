2fea09b949eed78a589ff83989217c72ddcf06da
janko12 <henrich.sury97@gmail.com>
2023-07-26 16:19:29 +0200

    Add App navbar header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { NavLink } from "react-router-dom";
+import { FaHome, FaShoppingCart } from "react-icons/fa";
+import styles from "./App.module.css";
+
 function App() {
-    return <div>App</div>;
+    return (
+        <div className="flex flex-col h-screen">
+            <header
+                className={`${styles.navbar} py-4 px-6 bg-slate-600 text-4xl text-white flex items-center gap-5`}
+            >
+                <NavLink to="/">
+                    <FaHome />
+                </NavLink>
+                <NavLink to="/shop" className="ml-auto text-3xl">
+                    Shop
+                </NavLink>
+                <NavLink to="/cart">
+                    <FaShoppingCart />
+                </NavLink>
+            </header>
+        </div>
+    );
 }
 
 export default App;
