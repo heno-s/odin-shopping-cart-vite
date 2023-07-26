70066ade2969874a52bafe8f30f714b08b90c3b0
janko12 <henrich.sury97@gmail.com>
2023-07-26 17:45:19 +0200

    Create homepage and style it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,20 @@
+import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
+
 export default function Home() {
-    return <div>Home</div>;
+    return (
+        <div
+            className={`${styles.container} flex items-center flex-col gap-6 justify-center`}
+        >
+            <span
+                className={`${styles["app-title"]} text-3xl text-center text-white`}
+            >
+                Explore Innovative Products
+            </span>
+
+            <Link to="/shop">
+                <button className="py-4 px-7 bg-white">Shop</button>
+            </Link>
+        </div>
+    );
 }
