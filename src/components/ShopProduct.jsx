6a07e95136b089ab1103e82ed331596f3aec4951
janko12 <henrich.sury97@gmail.com>
2023-07-31 14:38:03 +0200

    Handle change of product count

--- src/components/ShopProduct.jsx
+++ src/components/ShopProduct.jsx
@@ -1,6 +1,26 @@
 import { number, string } from "prop-types";
+import { useEffect, useState } from "react";
 
 export default function ShopProduct({ id, image, title, price }) {
+    const [productCount, setProductCount] = useState("1");
+    useEffect(() => {
+        if (+productCount < 1) {
+            setProductCount("1");
+        }
+    }, [productCount]);
+
+    function incrementCount() {
+        setProductCount((+productCount + 1).toString());
+    }
+
+    function handleSetProductCount(evt) {
+        setProductCount(evt.target.value);
+    }
+
+    function decrementCount() {
+        setProductCount((+productCount - 1).toString());
+    }
+
     return (
         <div className="flex flex-col gap-3 items-center px-5 justify-center">
             <span className="font-bold text-center">{title}</span>
@@ -12,9 +32,18 @@ export default function ShopProduct({ id, image, title, price }) {
             <strong className="text-3xl">{price} €</strong>
             <form className="flex justify-between w-[50%] text-xl gap-4">
                 <div className="flex text-2xl gap-2">
-                    <button type="button">-</button>
-                    <input type="number" className="w-[2ch]" />
-                    <button type="button">+</button>
+                    <button onClick={decrementCount} type="button">
+                        -
+                    </button>
+                    <input
+                        value={productCount}
+                        onChange={handleSetProductCount}
+                        type="number"
+                        className="w-[2ch] text-center"
+                    />
+                    <button onClick={incrementCount} type="button">
+                        +
+                    </button>
                 </div>
 
                 <button className="py-2 px-4 border-black border-x-2 border-y-2">
