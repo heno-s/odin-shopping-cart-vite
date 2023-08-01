242e47ff63064cb2a95f2f9e0cf1320990b6275c
janko12 <henrich.sury97@gmail.com>
2023-08-01 14:54:00 +0200

    Add rendering condition when cart items are empty

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -36,6 +36,23 @@ export default function Cart() {
         }
     }
 
+    function getTotalPrice() {
+        return cartItems.reduce((total, cartItem) => {
+            const product = products.find(
+                (product) => product.id === cartItem.id
+            );
+            return total + cartItem.quantity * product.price;
+        }, 0);
+    }
+
+    if (cartItems.length === 0) {
+        return (
+            <span className="text-center mt-6 font-bold">
+                No items in Cart
+            </span>
+        );
+    }
+
     if (productsLoading) {
         return <h1>Loading products...</h1>;
     }
@@ -45,28 +62,39 @@ export default function Cart() {
     }
 
     return (
-        <div className="grid grid-cols-[repeat(auto-fill,300px)] gap-10 justify-center items-start pt-20 overflow-auto">
-            {cartItems.map((cartProduct) => {
-                const product = products.find(
-                    (product) => cartProduct.id === product.id
-                );
-
-                const productsJSX = [];
-
-                for (let i = 0; i < cartProduct.quantity; i++) {
-                    productsJSX.push(
-                        <CartProduct
-                            handleDeleteFromCart={
-                                handleDeleteFromCart
-                            }
-                            key={product.id + i}
-                            {...product}
-                        />
+        <>
+            <div className="grid grid-cols-[repeat(auto-fill,300px)] gap-10 justify-center items-start pt-20 overflow-auto">
+                {cartItems.map((cartProduct) => {
+                    const product = products.find(
+                        (product) => cartProduct.id === product.id
                     );
-                }
 
-                return productsJSX;
-            })}
-        </div>
+                    const productsJSX = [];
+
+                    for (let i = 0; i < cartProduct.quantity; i++) {
+                        productsJSX.push(
+                            <CartProduct
+                                handleDeleteFromCart={
+                                    handleDeleteFromCart
+                                }
+                                key={product.id + i}
+                                {...product}
+                            />
+                        );
+                    }
+
+                    return productsJSX;
+                })}
+            </div>
+
+            <div className="flex flex-col gap-3 items-center mt-8">
+                <span className="text-xl">
+                    Total: <strong>{getTotalPrice()} €</strong>
+                </span>
+                <button className="py-2 px-5 bg-red-500 text-white text-2xl">
+                    Pay
+                </button>
+            </div>
+        </>
     );
 }
